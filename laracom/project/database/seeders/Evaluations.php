94d7c2612edf09775d9fb399ebfa8960b92c6f5f
amine-d <amine.doublali@g.dreamcareer.co.jp>
2024-02-16 16:32:15 +0900

    seederファイルの編集

--- laracom/project/database/seeders/Evaluations.php
+++ laracom/project/database/seeders/Evaluations.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Facades\DB;
 
-class EvaluationSeeder extends Seeder
+class Evaluations extends Seeder
 {
     /**
      * Run the database seeds.
